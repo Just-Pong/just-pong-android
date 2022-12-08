e95c803c2f24d6d4a498445657ba130104924b72
BHunor <burszanhunor@gmail.com>
2022-12-08 16:29:55 +0200

    Sending movement data to the server

--- JustPongApp/app/src/main/java/edu/sapi/justpongapp/backend/models/Message.kt
+++
@@ -1,7 +0,0 @@
-package edu.sapi.justpongapp.backend.models
-
-data class Message(private val message: String): BaseEntity() {
-    override fun toJson(): String {
-        return gson.toJson(this)
-    }
-}
